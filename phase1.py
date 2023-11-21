6ebf316904b8e5355b30c861ac3549137278eeae
TheIeagle <anelm9@ulaval.ca>
2023-11-20 22:42:33 -0500

    abdelwahedmigos

--- phase1.py
+++ phase1.py
@@ -1,7 +1,7 @@
 import argparse
-import requests
-import json
 from datetime import datetime
+import json
+import requests
 
 def analyser_commande():
     parser = argparse.ArgumentParser(description="Extraction de valeurs historiques pour un ou plusieurs symboles boursiers.")
@@ -18,9 +18,9 @@ def analyser_commande():
             args.debut = args.fin
         if not args.fin:
             args.fin = datetime.now().date()
-        
+
         historique = produire_historique(symbole, args.debut, args.fin, args.valeur)
-        
+
         print(f'titre={symbole}: valeur={args.valeur}, début=datetime.date({args.debut.year}, {args.debut.month}, {args.debut.day}), fin=datetime.date({args.fin.year}, {args.fin.month}, {args.fin.day})')
         historique_formatte = [(f'datetime.date({d.year}, {d.month}, {d.day})', v) for d, v in historique]
         print(historique_formatte)
@@ -28,21 +28,20 @@ def analyser_commande():
 def produire_historique(symbole, debut, fin, valeur):
     url = f'https://pax.ulaval.ca/action/{symbole}/historique/'
     params = {'début': debut.strftime('%Y-%m-%d'), 'fin': fin.strftime('%Y-%m-%d')}
-    
+
     reponse = requests.get(url=url, params=params)
     donnees = json.loads(reponse.text)
-    
+
     if 'message d\'erreur' in donnees:
         raise ValueError(donnees['message d\'erreur'])
-    
-    historique = donnees['historique']
-    
-    resultats = [(datetime.strptime(date, '%Y-%m-%d').date(), day_data[valeur]) 
-                 for date, day_data in historique.items() if valeur in day_data]
-    
-    resultats.sort(key=lambda x: x[0])
-    
-    return resultats
 
+    historique = donnees['historique']
+
+    resultats = [(datetime.strptime(date, '%Y-%m-%d').date(), day_data[valeur])
+                 for date, day_data in historique.items() if valeur in day_data]
+
+    resultats.sort(key=lambda x: x[0])
+
+    return resultats
 
 analyser_commande()
